44a4ddb2ac4cb2c0d48a9766b2027951f35e03d6
Karhik R <karhikr@LBMAC-12436.local>
2025-04-10 22:46:35 +0530

    bfix

--- src/managers/AuthManager.ts
+++ src/managers/AuthManager.ts
@@ -2,7 +2,7 @@ import { getAccessToken, getProfileData } from "../utils/instagramApi";
 import { InstagramUser } from "../models/InstagramUser";
 
 export const getAuthUrl = (): string => {
-  return `https://api.instagram.com/oauth/authorize?client_id=${process.env.INSTAGRAM_CLIENT_ID}&redirect_uri=${process.env.INSTAGRAM_REDIRECT_URI}&scope=user_profile&response_type=code`;
+  return `https://api.instagram.com/oauth/authorize?client_id=${process.env.INSTAGRAM_CLIENT_ID}&redirect_uri=${process.env.INSTAGRAM_REDIRECT_URI}&response_type=code`;
 };
 export const handleAuthCallback = async (
   code: string
