03a42ace772460c26511aace317e5cf5be187772
Karhik R <karhikr@LBMAC-12436.local>
2025-04-11 12:46:44 +0530

    scope changes

--- src/managers/AuthManager.ts
+++ src/managers/AuthManager.ts
@@ -2,7 +2,10 @@ import { getAccessToken, getProfileData } from "../utils/instagramApi";
 import { InstagramUser } from "../models/InstagramUser";
 
 export const getAuthUrl = (): string => {
-  return `https://api.instagram.com/oauth/authorize?client_id=${process.env.INSTAGRAM_CLIENT_ID}&redirect_uri=${process.env.INSTAGRAM_REDIRECT_URI}&scope=user_profile,user_media&response_type=code`;
+  return `https://api.instagram.com/oauth/authorize?client_id=${process.env.INSTAGRAM_CLIENT_ID}&redirect_uri=${process.env.INSTAGRAM_REDIRECT_URI}&scope=instagram_business_basic,
+instagram_business_content_publish,
+instagram_business_manage_messages,
+instagram_business_manage_comments&response_type=code`;
 };
 export const handleAuthCallback = async (
   code: string
